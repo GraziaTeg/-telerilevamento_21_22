7a5c029feb44733cb77e48443e048ba1a02d872f
GraziaTeg <101199417+GraziaTeg@users.noreply.github.com>
2022-05-10 20:55:26 +0200

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -1,4 +1,13 @@
+#04.03.22
 # Questo è il primo script che useremo a lezione
 
-#prima funzione che useremo 
+#prima funzione che useremo
+#richiamiamo la libreria raster
 library(raster)
+
+#10.03.22
+#richiamiamo la libreria raster
+#installazione
+#install.packages("raster")[apriamo le virgolette perchè siamo usciti da R]
+library(raster)
+#
