436cf78aebbb74179baf86dd5f9e994b5ea6d9af
GraziaTeg <101199417+GraziaTeg@users.noreply.github.com>
2022-05-31 16:11:28 +0200

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -16,4 +16,12 @@ library(raster)
 #setwd("/User/utente/lab") #mac
 setwd("C:/lab/") #windows
 
-
+#import
+#per importare un pacchetto di dati esiste una funzione in R
+#la funzione per importare tanti dati in R si chiama BRICK
+#come si usano le funzioni su R
+brick("p224r63_2011.grd")
+#poi l'assegnamo all'oggetto
+l2011 <- brick("p224r63_2011.grd")
+#dopo aver importato i dati = diamo il nome l2011 e usciranno tutte le informazioni 
+l2011
