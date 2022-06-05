897ea85e3ada8c6a388c80f1354deec6b513c35d
GraziaTeg <101199417+GraziaTeg@users.noreply.github.com>
2022-06-05 16:00:54 +0200

    Update R_code_spectral_indices.r

--- R_code_spectral_indices.r
+++ R_code_spectral_indices.r
@@ -12,3 +12,25 @@ brick("defor1_.jpg")
 l1992 <- brick("defor1_.jpg")
 l1992
 
+#Indice di SHANNON = BIT
+2^8 #2 al 8
+#[1] 256
+#o da 0 a 255 = 256
+#o da 1 a 256 = 256
+#molti immagini satellitari vanno da 0 a 255 = 8 BIT = 
+#quindi utilizziamo solo dati interi 
+
+?plotRGB #del pacchetto raster
+plotRGB(l1992, r = 1, g = 2, b = 3, stretch = "lin")
+
+#layer 1 = NIR = prima banda
+#layer 2 = red
+#layer 3 = green
+
+#esercizio: 
+#import the second file -> defor2_.jpg -> give it the name l2006
+brick("defor2_.jpg")
+l2006 <- brick("defor2_.jpg")
+l2006
+plotRGB(l2006, r = 1, g = 2, b = 3, stretch = "lin")
+
