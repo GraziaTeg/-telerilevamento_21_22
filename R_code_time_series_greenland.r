3a042b6fd092cfc3fb04f588040923a10c1852e2
GraziaTeg <101199417+GraziaTeg@users.noreply.github.com>
2022-06-06 19:56:55 +0200

    Update R_code_time_series_greenland.r

--- R_code_time_series_greenland.r
+++ R_code_time_series_greenland.r
@@ -5,3 +5,31 @@ library(raster)
 #cartella di lavoro 
 setwd("D:/UNIVERSITA' MAGISTRALE/1 ANNO/2 SEMESTRE/TELERILEVAMENTO GEO-ECOLOGICO/R/3. 10.03.22/lab/greenland")
 #aggiungere anche la cartella greenland
+
+?raster
+
+#importiamo immagine 2000
+raster("lst_2000.tif")
+lst2000 <- raster("lst_2000.tif")
+lst2000
+#plottiamo 
+plot(lst2000)
+
+#Exercise: import all the data 
+#importiamo tutti i dati del 2005, 2010, 2015
+#2005
+raster("lst_2005.tif")
+lst2005 <- raster("lst_2005.tif")
+lst2005
+#2010
+raster("lst_2010.tif")
+lst2010 <- raster("lst_2010.tif")
+lst2010
+#2015
+raster("lst_2015.tif")
+lst2015 <- raster("lst_2015.tif")
+lst2015
+
+
+
+
