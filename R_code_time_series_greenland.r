5c3468acc7384562460ca755e648f45ba83e5999
GraziaTeg <101199417+GraziaTeg@users.noreply.github.com>
2022-06-06 17:50:42 +0200

    Update and rename R_code_time_series.r to R_code_time_series_greenland.r

---
+++ R_code_time_series_greenland.r
@@ -0,0 +1,7 @@
+#Time series analysis of Greenland LST data 
+#Analisi delle serie temporali dei dati LST della Groenlandia
+#inserire pacchetti 
+library(raster)
+#cartella di lavoro 
+setwd("D:/UNIVERSITA' MAGISTRALE/1 ANNO/2 SEMESTRE/TELERILEVAMENTO GEO-ECOLOGICO/R/3. 10.03.22/lab/greenland")
+#aggiungere anche la cartella greenland
