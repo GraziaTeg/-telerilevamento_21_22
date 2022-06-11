45d0229e6a1a9441bc7797e88f4b81b9587cd510
GraziaTeg <101199417+GraziaTeg@users.noreply.github.com>
2022-06-11 14:40:36 +0200

    Update 7. R_code_multivariate_analysis.r

--- 7. R_code_multivariate_analysis.r
+++ 7. R_code_multivariate_analysis.r
@@ -1,1 +1,16 @@
+#R code for multivariate analysis
+
+#quali libreria usiamo:
+library(raster)
+library(RStoolbox)#contiene le funzione per fare l'analisi multivariata
+
+#settaggio della cartella 
+setwd("D:/UNIVERSITA' MAGISTRALE/1 ANNO/2 SEMESTRE/TELERILEVAMENTO GEO-ECOLOGICO/R/3. 10.03.22/lab")
+
+#le bande che andremo a utilizzare sono quelle del
+#p224r63 #immagini del Landsat
+#path del satellite ogni path ha un numero con righe = battaglia navale
+brick("p224r63_2011_masked.grd")#altro formato rispetto a quello utilizzato 
+p224r63_2011 <- brick("p224r63_2011_masked.grd")
+p224r63_2011
 
