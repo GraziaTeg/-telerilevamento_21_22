5710a1216cc9a7e8efd1cec2dfea1db37614ed14
GraziaTeg <101199417+GraziaTeg@users.noreply.github.com>
2022-06-13 16:08:19 +0200

    Update 9. R_code_variability_3.r

--- 9. R_code_variability_3.r
+++ 9. R_code_variability_3.r
@@ -1,1 +1,9 @@
+#19.05.22
 
+#R code variability 3 - using PC components
+library(raster)
+library(RStoolbox)
+library(ggplot2)
+library(patchwork)
+
+setwd("D:/UNIVERSITA' MAGISTRALE/1 ANNO/2 SEMESTRE/TELERILEVAMENTO GEO-ECOLOGICO/R/3. 10.03.22/lab")
