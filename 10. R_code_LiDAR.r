0a1f61d7e98744f126443e15120ba6850c8015d9
GraziaTeg <101199417+GraziaTeg@users.noreply.github.com>
2022-06-13 18:51:16 +0200

    Create 10. R_code_LiDAR.r

---
+++ 10. R_code_LiDAR.r
@@ -0,0 +1,5 @@
+#R code for visualising and analysing LiDAR data
+#Codice R per la visualizzazione e l'analisi dei dati LiDAR
+
+#settaggio della Working Directory
+setwd("D:/UNIVERSITA' MAGISTRALE/1 ANNO/2 SEMESTRE/TELERILEVAMENTO GEO-ECOLOGICO/R/3. 10.03.22/lab")
